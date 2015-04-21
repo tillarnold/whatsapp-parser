f0273c50b6cdf5e596fe4de980619bf2b9ba4182
tillarnold <throwable42@gmail.com>
2015-04-21 22:51:17 +0200

    Prebuild the parser

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 'use strict'
 
-var peg = require('pegjs')
-  , fs = require('fs')
-  , path = require('path')
+var readFileSync = require('fs').readFileSync
+  , parser = require('./chatParser.js')
 
-var readFileSync = fs.readFileSync
-  , parser = peg.buildParser(readFileSync(path.join(__dirname, '/chat.pegjs'), 'utf8'))
-  , parse = function parse (text) {
+var parse = function parse (text) {
       return parser.parse(text.replace(/^\ufeff/g, ''))
     }
   , parseFile = function parseFile (file) {
